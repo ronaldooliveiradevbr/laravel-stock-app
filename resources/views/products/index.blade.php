16b30f6e56842fc734bf1b400d9301a03e89d91d
Ronaldo Oliveira <rfdeoliveira.pmsp@gmail.com>
2019-03-21 07:18:55 -0300

    Add link to product creation form

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -9,33 +9,44 @@
   </div>    
 @endif
 
-<table class="table">
-  <thead>
-    <tr>
-      <th>Name</th>
-      <th>Price</th>
-      <th>Quantity</th>
-      <th>Actions</th>
-    </tr>
-  </thead>
-  <tbody>
-  @foreach ($products as $product)
-    <tr class="{{ $product->quantity <= 1 ? 'alert-danger': '' }}">
-      <td>{{ $product->name }}</td>
-      <td>{{ $product->price }}</td>
-      <td>{{ $product->quantity }}</td>
-      <td>
-        <a href="/products/{{ $product->id }}" class="btn btn-link">show</a>
-        <a href="/products/{{ $product->id }}/edit" class="btn btn-link">edit</a>
-	<form method="post" action="/products/{{ $product->id }}" class="form-inline">
-          @csrf
-          @method('DELETE')
-          <button type="submit" class="btn btn-link">delete</button>
-        </form>
-      </td>
-    </tr>
-  @endforeach
-  </tbody>
-</table>
+<div class="row">
+  <div class="col-md-12">
+    <a href="/products/create" class="btn btn-primary">Add product</a>
+  </div>
+</div>
+<br>
+
+<div class="row">
+  <div class="col-md-12">
+    <table class="table">
+      <thead>
+	<tr>
+	  <th>Name</th>
+	  <th>Price</th>
+	  <th>Quantity</th>
+	  <th>Actions</th>
+	</tr>
+      </thead>
+      <tbody>
+      @foreach ($products as $product)
+        <tr class="{{ $product->quantity <= 1 ? 'alert-danger': '' }}">
+          <td>{{ $product->name }}</td>
+          <td>{{ $product->price }}</td>
+          <td>{{ $product->quantity }}</td>
+          <td>
+            <a href="/products/{{ $product->id }}" class="btn btn-link">show</a>
+            <a href="/products/{{ $product->id }}/edit" class="btn btn-link">edit</a>
+	    <form method="post" action="/products/{{ $product->id }}" class="form-inline">
+              @csrf
+              @method('DELETE')
+              <button type="submit" class="btn btn-link">delete</button>
+            </form>
+          </td>
+	</tr>
+      @endforeach
+      </tbody>
+    </table>
+  </div>
+</div>
 @endsection
 
